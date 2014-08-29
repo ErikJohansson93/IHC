48445d52b47dad731fbbcad66e35df2b755671da
Erik Johansson <erik.johansson@oddhill.se>
2014-08-29 13:59:29 +0200

    Added support for node-webkit

---
+++ js/win.js
@@ -0,0 +1,24 @@
+var gui = global.window.nwDispatcher.requireNwGui();
+var win = gui.Window.get();
+
+var tray = new gui.Tray({title: 'IHC'});
+var menu = new gui.Menu();
+menu.append(new gui.MenuItem({ label: 'Show' }));
+menu.append(new gui.MenuItem({ label: 'Hide' }));
+menu.append(new gui.MenuItem({ label: 'Quit' }));
+
+menu.items[0].click = function() {
+  window.moveTo(9999,0);
+  win.show();
+};
+
+menu.items[1].click = function() {
+  win.hide();
+};
+
+menu.items[2].click = function() {
+  gui.App.quit();
+};
+
+tray.menu = menu;
+
